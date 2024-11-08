6b2a8a3ef8c37765995b21dc746a3249444f4a65
tmelachos <tmelachos@google.com>
2024-11-08 11:09:48 -0300

    Article summary step and tts version update
    Change-Id: I219e86e62925ca194d2f7c64c76e55090554ba17

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,7 @@ from tests.audio.text_to_speech_step_test import TextToSpeechStepTest
 from tests.pipeline.ffmpeg_step_test import FfmpegStepTest
 from tests.pipeline.video_generation_context_test import VideoGenerationContextTest
 from tests.pipeline.video_generation_pipeline_test import VideoGenerationPipelineTest
+from tests.text.summarize_text_step_test import SummarizeTextStepTest
 from tests.util.create_workdir_step_test import CreateWorkdirStepTest
 from tests.video.add_logo_to_video_step_test import AddLogoToVideoStepTest
 from tests.video.add_title_to_video_step_test import AddTitleToVideoStepTest
--- text/__init__.py
+++ text/__init__.py
@@ -1,3 +1,3 @@
 """Module for pipeline steps that change the text."""
 from text import article_fetch_gcp_step
-
+from text import summarize_text_step
